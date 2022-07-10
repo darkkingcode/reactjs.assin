100d780a503371306c6cb1c67bfcd5a6fe5bf952
darkkingcode <109023995+darkkingcode@users.noreply.github.com>
2022-07-10 18:16:40 +0530

    Update Academic.js

--- src/components/Academic/Academic.js
+++ src/components/Academic/Academic.js
@@ -9,17 +9,17 @@ import './Academic.css';
             <div className='clg-btn'>
                     <h3>Lovely Professional University</h3>
                     <h4>B.Tech (computer science and engineering)</h4>
-                    <h5>CGPA:- 8.48 (2019-present)</h5>
+                    <h5>CGPA:- 7.52 (2019-present)</h5>
             </div>
             <div className='clg-btn'>
                     <h3>Sri chaitanya junior college</h3>
                     <h4>XII (Board of Intermediate Education)</h4>
-                    <h5>CGPA:- 9.94 (2017-2019)</h5>
+                    <h5>CGPA:- 68% (2017-2019)</h5>
             </div>
             <div className='clg-btn'>
                     <h3>Bhashyam High School</h3>
                     <h4>X( Board of Secondary Education)</h4>
-                    <h5>CGPA:- 9.7 (2016-2017)</h5>
+                    <h5>CGPA:- 9.4 (2016-2017)</h5>
             </div>
             </section>
 
